d5d7ed6fa904565a975b72e48a050c5923c6a510
Andrey Kalyamin <kalyamin.andrey@gmail.com>
2021-02-08 10:48:21 +0400

    added icons Launch Screen Rows height

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -29,12 +29,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
             
-            cell?.textLabel?.text = restaurantNames[indexPath.row]
-            cell?.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
-            return cell!
+            cell.textLabel?.text = restaurantNames[indexPath.row]
+            cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+            cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+            cell.imageView?.clipsToBounds = true
+            return cell
         }
+    
+    // MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
 }
 
